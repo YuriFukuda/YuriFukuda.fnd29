5fb4c720164d09f39501e9992b6ff50f5ff501ad
YuriFukuda <yuri@mail.toyota.co.jp>
2024-09-01 18:24:23 +0900

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,9 +1,11 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
+//リンクをクリックすると、リンク先外部サイトを、別のWINDOWで開く
+
+
 
 //テストの定型文 Test boilerplate
-
 function test(actual, expected) {
     if (JSON.stringify(actual) === JSON.stringify(expected)) {
       console.log("OK! Test PASSED.");
@@ -15,16 +17,4 @@ function test(actual, expected) {
     }
   }
 
-
-/**
- * ★What★
- *  
- * 
- * ★can I★
- * 
- * 
- * 
- * ★Do★
- * 
- * 
- */
+  //https://yurifukuda.github.io/YuriFukuda.fnd29/ Gitの保存場所URLメモ
