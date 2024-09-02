6bfb33e81f48d81004518b905d0707035f35ca31
YuriFukuda <yuri@mail.toyota.co.jp>
2024-09-02 16:50:43 +0900

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -1,20 +1,13 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
-//リンクをクリックすると、リンク先外部サイトを、別のWINDOWで開く
+//リンクをクリックするイベント
+//別のWINDOWを開かないようにデフォルト動作？をとめる
+//今あるWINDOWへリンク先を開く
 
+function jumpTo(panasonicSite){
+  event.preventDefault();
+  const url = event.currentTarget.href;
+  window.open(url, `_blank`);
 
-
-//テストの定型文 Test boilerplate
-function test(actual, expected) {
-    if (JSON.stringify(actual) === JSON.stringify(expected)) {
-      console.log("OK! Test PASSED.");
-    } else {
-      console.error("Test FAILED. Try again!");
-      console.log("    actual: ", actual);
-      console.log("  expected: ", expected);
-      console.trace();
-    }
-  }
-
-  //https://yurifukuda.github.io/YuriFukuda.fnd29/ Gitの保存場所URLメモ
+}
